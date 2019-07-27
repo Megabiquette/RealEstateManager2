24f0b94d4662720b82f868fd191f27fffa8866fe
Megabiquette <megabiquette@gmail.com>
2019-07-27 14:11:38 +0200

    First Commit. Added TravisCI and improved Utils class.

---
+++ app/src/main/java/com/albanfontaine/realestatemanager2/Controllers/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.albanfontaine.realestatemanager2.Controllers
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.albanfontaine.realestatemanager2.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
